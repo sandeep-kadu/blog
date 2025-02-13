2e0ad3c54c7460c0cd006608e8d1415f4ef02a43
sandeep kadu <sandeepkadu41@gmail.com>
2025-02-13 21:19:49 +0530

    to do app added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -77,4 +77,20 @@ toggle()
   this.display=!this.display;
 }
 
+list:any[]=[];
+addTask(item:string)
+{
+  //console.warn(item);
+  this.list.push({id:this.list.length,name:item});
+  console.warn(this.list);
+}
+removeTask(id:number)
+{
+  console.warn(id);
+  this.list=this.list.filter(item=>item.id!==id);
+}
+
+
+
+
 }
